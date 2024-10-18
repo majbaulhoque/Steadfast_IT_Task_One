5165b1cd173aed451991b2ca6211ac286f75e1f9
Majba UL Hoque <majba9399@gmail.com>
2024-10-18 19:14:01 +0600

    create modal

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ function toggleMenu(){
         menuIcon.classList.add("fa-bars");
     }
 }
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("openModalBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block"; 
+}
+
+span.onclick = function() {
+    modal.style.display = "none"
+}
+
+window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display = "none"
+    }
+}
